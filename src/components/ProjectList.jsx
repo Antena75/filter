5fc2c049686246bdbfff0b8cad88698d05b6d8ad
Antena <a.korotaev75@gmail.com>
2025-07-23 08:27:00 +0500

    Initial commit

---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,13 @@
+const ProjectList = ({ projects }) => {
+    return (
+        <div className="project-list">
+            {projects.map((project, index) => (
+                <div key={index} className="project-item">
+                    <img src={project.img} alt={project.category} />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ProjectList;
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,13 @@
+const Toolbar = ({ filters, selected, onSelectFilter }) => {
+    return (
+        <div className="toolbar">
+            {filters.map(filter => (
+                <button key={filter} className={filter === selected ? 'selected' : ''} onClick={() => onSelectFilter(filter)}>
+                    {filter}
+                </button>
+            ))}
+        </div>
+    );
+};
+
+export default Toolbar;
